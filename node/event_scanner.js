854deb9bff21c69fe07df4d344a017b2cc263a4a
tomi <tomilui1221@gmail.com>
2022-01-22 22:17:56 -0800

    update

--- node/event_scanner.js
+++ node/event_scanner.js
@@ -2,58 +2,39 @@
 import Web3 from 'web3';
 import dotenv from 'dotenv'
 import { ProgressBar } from "./progressBar.js"
+import { ABI } from "./constants.js"
 dotenv.config()
 
 import fs from 'fs'
 
 var file = fs.createWriteStream('test.txt');
-file.on('error', function(err) { 
+file.on('error', function (err) {
     console.log("Cannot open file error");
     process.exit(1)
- });
+});
 
-const web3 = new Web3(process.env["ETH_ENDPOINT_URL"]);
+let web3
 
-const YEAR_ADDRESS = "0x9010a15184da16e3a7c5b4aa50094dfe3bb36989"
-const TOKEN_ADDRESS = YEAR_ADDRESS
-const ABI = [
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "name": "value",
-                "type": "uint256"
-            }
-        ],
-        "name": "Transfer",
-        "type": "event"
-    }
-]
+/*
+scripts:
+node ./event_scanner.js eth 0x9010a15184da16e3a7c5b4aa50094dfe3bb36989 13910234 13910334 10
+node ./event_scanner.js bsc 0x319c706a32e4c34578487c234d5162bed1260c08 14082226 14323552 100
+*/
+const args = process.argv.slice(2)
+const fromBlock = parseInt(args[2])
+const toBlock = parseInt(args[3])
+const TOKEN_ADDRESS = args[1]
+const BLOCK_STEP = args[4]? parseInt(args[4]) : 10;
+const EventType = args[5]? args[5] : "Transfer"
+
+switch (args[0].toLowerCase()) {
+    case "bsc":
+        web3 = new Web3(process.env["BSC_ENDPOINT_URL"]);
+    case "eth":
+        web3 = new Web3(process.env["ETH_ENDPOINT_URL"]);
+}
+
 const myContract = new web3.eth.Contract(ABI, TOKEN_ADDRESS);
-
-// const currentBlock = await web3.eth.getBlockNumber()
-
-// def get_block_timestamp(self, block_num) -> datetime.datetime:
-// """Get Ethereum block timestamp"""
-// try:
-//     block_info = self.web3.eth.getBlock(block_num)
-// except BlockNotFound:
-//     # Block was not mined yet,
-//     # minor chain reorganisation?
-//     return None
-// last_time = block_info["timestamp"]
-// return datetime.datetime.utcfromtimestamp(last_time)
 
 async function getBlockTimestamp(blockNum) {
     try {
@@ -64,35 +45,35 @@ async function getBlockTimestamp(blockNum) {
     }
 }
 
-async function scan(fromBlock, lastBlock) {
+async function scan(fromBlock, lastBlock, BLOCK_STEP=10, eventType) {
 
     console.log("Scanning Events...")
-    const BLOCK_STEP = 10
 
-    const event_list = {}   
+    const event_list = {}
     let currentBlock = fromBlock
 
     // const progressBar = new ProgressBar(start, end)
 
     for (; currentBlock < lastBlock; currentBlock += BLOCK_STEP) {
-
-        await myContract.getPastEvents('Transfer', {
+        console.log(currentBlock);
+        await myContract.getPastEvents(eventType, {
             fromBlock: currentBlock - BLOCK_STEP,
             toBlock: currentBlock
 
         }, function (error, events) {
             if (error) {
-                console.log("error encountered")
+                console.log("error encountered while fetching events")
                 // progressBar.stop()
                 return event_list
             }
             // console.log(events)
-            events.forEach( e => {
+            // console.log("logging event: ", events);
+            events.forEach(e => {
                 console.log(e);
                 // console.log(e.raw.topics);
                 file.write(JSON.stringify(e.returnValues, null, 2))
             })
-            event_list.push(...events)
+            // event_list.push(...events)
             // progressBar.update(BLOCK_STEP)
         })
     }
@@ -100,8 +81,45 @@ async function scan(fromBlock, lastBlock) {
     return event_list
 }
 
+async function scanAllEvents(fromBlock, lastBlock, BLOCK_STEP=10, eventType) {
+
+    console.log("Scanning Events...")
+
+    const event_list = {}
+    let currentBlock = fromBlock
+
+    // const progressBar = new ProgressBar(start, end)
+
+    for (; currentBlock < lastBlock; currentBlock += BLOCK_STEP) {
+        console.log(currentBlock);
+        await myContract.events.getPastEvents(eventType, {
+            fromBlock: currentBlock - BLOCK_STEP,
+            toBlock: currentBlock
+
+        }, function (error, events) {
+            if (error) {
+                console.log("error encountered while fetching events")
+                // progressBar.stop()
+                return event_list
+            }
+            // console.log(events)
+            // console.log("logging event: ", events);
+            events.forEach(e => {
+                console.log(e);
+                // console.log(e.raw.topics);
+                file.write(JSON.stringify(e.returnValues, null, 2))
+            })
+            // event_list.push(...events)
+            // progressBar.update(BLOCK_STEP)
+        })
+    }
+    // progressBar.stop()
+    return event_list
+}
+
+
 // const event_list = await scan(13910234, 13929020)
-const event_list = await scan(13910234, 13910304)
+const event_list = await scan(fromBlock, toBlock, BLOCK_STEP)
 // console.log(event_list)
 
 file.end()

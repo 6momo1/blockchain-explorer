ed69de7d26f1b198955f5bab461baee8df54bf22
tomi <tomilui1221@gmail.com>
2022-01-24 19:57:47 -0800

    refractored

--- ts/event_scanner.ts
+++ ts/event_scanner.ts
@@ -2,9 +2,10 @@
 import Web3 from 'web3';
 import dotenv from 'dotenv'
 import { ProgressBar } from "./progressBar"
-import {ABI} from "./constants"
+import {ERC20ABI, UNIABI} from "./abis"
+import { AbiItem } from 'web3-utils'
 dotenv.config()
-import { Event } from './types.js';
+import { SwapEvent } from './types.js';
 import fs from 'fs'
 
 var file = fs.createWriteStream('test.txt');
@@ -12,33 +13,6 @@ file.on('error', function (err) {
     console.log("Cannot open file error");
     process.exit(1)
 });
-
-
-/*
-scripts:
-node ./event_scanner.js eth 0x9010a15184da16e3a7c5b4aa50094dfe3bb36989 13910234 13910334 10
-node ./event_scanner.js bsc 0x319c706a32e4c34578487c234d5162bed1260c08 14082226 14323552 100 allEvents
-million token:
-eth:
-node ./event_scanner.js eth 0x6b4c7a5e3f0b99fcd83e9c089bddd6c7fce5c611 14020249 14057732 100 allEvents
-bsc:
-node ./event_scanner.js bsc 0xBF05279F9Bf1CE69bBFEd670813b7e431142Afa4 9219811 9230089 100 allEvents
-lp:
-node ./event_scanner.js bsc 0x7cb5e7048215f7c225a8248b4c33fd32ca579c75 14534889 14547545 1000 allEvents
-
-theoreum:
-node ./event_scanner.js bsc 0x10ED43C718714eb63d5aA57B78B54704E256024E 14624501 14624502 1 allEvents
-
-
-PANCAKE SWAP:
-0x67606A460e12c71ce323076A17d8796e5D39e372 # spider doge
-node ./event_scanner.js bsc 0x67606A460e12c71ce323076A17d8796e5D39e372 14425575 14425775 100 allEvents
-
-0x9A1071d17b8126679Aeca3EF152F784bca339c3A # YEAR
-node ./event_scanner.js bsc 0x9A1071d17b8126679Aeca3EF152F784bca339c3A 13911535 13912106 100 Swap
-
-*/
-
 
 const args = process.argv.slice(2)
 const fromBlock = parseInt(args[2])
@@ -55,14 +29,14 @@ switch (args[0].toLowerCase()) {
         web3 = process.env["ETH_ENDPOINT_URL"]? new Web3(process.env["ETH_ENDPOINT_URL"]): undefined;
 }
 
-const myContract = new web3.eth.Contract(ABI, TOKEN_ADDRESS);
+const myContract = new web3.eth.Contract(UNIABI as AbiItem[], TOKEN_ADDRESS);
 
 
-async function scan(fromBlock:number, lastBlock:number, BLOCK_STEP=10, eventType:string): Promise<Event[]> {
+async function scan(fromBlock:number, lastBlock:number, BLOCK_STEP=10, eventType:string): Promise<SwapEvent[]> {
 
     console.log("Scanning Events...")
 
-    const event_list: Event[] = []
+    const event_list: SwapEvent[] = []
     let currentBlock = fromBlock
 
     const progressBar = new ProgressBar(0, lastBlock - fromBlock)
@@ -72,7 +46,7 @@ async function scan(fromBlock:number, lastBlock:number, BLOCK_STEP=10, eventType
             fromBlock: currentBlock - BLOCK_STEP,
             toBlock: currentBlock
 
-        }, function (error, events: Event[]) {
+        }, function (error, events: any) {
             if (error) {
                 progressBar.stop()
                 console.log("error encountered while fetching events")
@@ -105,12 +79,12 @@ const main = async () => {
     await scan(fromBlock, toBlock, BLOCK_STEP, EventType)
         .then( res => {
             res.forEach( event => {
-                console.log(event.transactionHash, event.raw.data, event.raw.topics);
+                // console.log(event.transactionHash, event.raw.data, event.raw.topics);
+                console.log(event.returnValues);
             })
         })
 
     file.end()
-
 }
 
 main()
--- ts/types.ts
+++ ts/types.ts
@@ -1,19 +1,34 @@
-export interface Event {
-    address: String,
-    blockHash: String,
-    blockNumber: number,
-    logIndex: number,
-    removed: Boolean,
-    transactionHash: String,
-    transactionIndex: number,
-    id: String,
-    returnValues: {
-        Result: {}
-    },
-    event: String,
-    signature: any,
-    raw: {
-      data: String,
-      topics: any[]
-    }
+export interface SwapEvent {
+  address: String,
+  blockHash: String,
+  blockNumber: number,
+  logIndex: number,
+  removed: Boolean,
+  transactionHash: String,
+  transactionIndex: number,
+  id: String,
+  returnValues: {
+    Result: SwapResult
+  },
+  event: String,
+  signature: any,
+  raw: {
+    data: String,
+    topics: any[]
+  }
+}
+
+export interface SwapResult {
+  '0': string,
+  '1': string,
+  '2': string,
+  '3': string,
+  '4': string,
+  '5': string,
+  sender: string,
+  amount0In: string,
+  amount1In: string,
+  amount0Out: string,
+  amount1Out: string,
+  to: string
 }

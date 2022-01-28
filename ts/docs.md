2347c0e2188dbb894608ae30f2b1c6b8067b715e
tomi <tomilui1221@gmail.com>
2022-01-27 20:23:00 -0800

    update

---
+++ ts/docs.md
@@ -0,0 +1,11 @@
+
+process:
+
+find out contract pair:
+- find out uniswap pair contract ( can be done manually )
+- figure out which is token1 and token0 ( can be done manually )
+- find out which block to sync prices from ( can be done manually )
+
+sync
+- to database
+- 
--- ts/event_scanner.ts
+++ ts/event_scanner.ts
@@ -2,11 +2,11 @@
 import Web3 from 'web3';
 import dotenv from 'dotenv'
 import { ProgressBar } from "./progressBar"
-import { ERC20ABI, UNIABI } from "./abis"
+import { ERC20ABI, TOKEN_PAIR_EXCHANGE_ABI, UNISWAP_FACTORY_ABI } from "./abis"
 import { AbiItem } from 'web3-utils'
 dotenv.config()
 import { SwapEvent } from './types.js';
-import fs from 'fs'
+import fs, { symlinkSync } from 'fs'
 
 var file = fs.createWriteStream('test.txt');
 file.on('error', function (err) {
@@ -21,6 +21,7 @@ const toBlock = parseInt(args[3])
 const TOKEN_ADDRESS = args[1]
 const BLOCK_STEP = args[4] ? parseInt(args[4]) : 10;
 const EventType = args[5] ? args[5] : "Transfer"
+const addressType = args[6] ? args[6] : "none"
 
 let web3: Web3
 if (chainSelected == "bsc") {
@@ -29,13 +30,33 @@ if (chainSelected == "bsc") {
     web3 = process.env["BSC_ENDPOINT_URL_1"] ? new Web3(process.env["BSC_ENDPOINT_URL_1"]) : undefined;
 }
 else if (chainSelected == "eth") {
-    web3 = process.env["ETH_ENDPOINT_URL"]? new Web3(process.env["ETH_ENDPOINT_URL"]): undefined;
+    web3 = process.env["ETH_ENDPOINT_URL"] ? new Web3(process.env["ETH_ENDPOINT_URL"]) : undefined;
+}
+else {
+    console.log("invalid Network");
+    process.exit(1)
 }
 
-const myContract = new web3.eth.Contract(UNIABI as AbiItem[], TOKEN_ADDRESS);
+let myContract
+const tokenExchangeContract = new web3.eth.Contract(TOKEN_PAIR_EXCHANGE_ABI as AbiItem[], TOKEN_ADDRESS);
+const uniswapFactoryContract = new web3.eth.Contract(UNISWAP_FACTORY_ABI as AbiItem[], TOKEN_ADDRESS);
+if (addressType == "uniFactory") {
+    myContract = uniswapFactoryContract
+}
+else if (addressType == "tokenExchange") {
+    myContract = tokenExchangeContract
+}
+else if (addressType == "address") {
+    console.log("not implemented yet");
+    process.exit(1)
+}
+else {
+    console.log("Invalid contract type.");
+    process.exit(1)
+}
 
 
-async function scan(fromBlock: number, lastBlock: number, BLOCK_STEP = 10, eventType="allEvents"): Promise<SwapEvent[]> {
+async function scan(fromBlock: number, lastBlock: number, BLOCK_STEP = 10, eventType = "allEvents", myContract): Promise<SwapEvent[]> {
 
     console.log("Scanning Events...")
 
@@ -71,7 +92,7 @@ async function scan(fromBlock: number, lastBlock: number, BLOCK_STEP = 10, event
 const main = async () => {
 
     // scan for events
-    await scan(fromBlock, toBlock, BLOCK_STEP, EventType)
+    await scan(fromBlock, toBlock, BLOCK_STEP, EventType, myContract)
         .then(res => {
             res.forEach(event => {
                 // console.log(event.transactionHash, event.raw.data, event.raw.topics);
